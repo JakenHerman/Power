9135db7c6595f87c9e49307200289ab800c4e71a
Jaken Herman <jch053@shsu.edu>
2015-03-05 10:47:47 -0600

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,23 @@
+public class Test {
+
+    public int power1(int x, int n){
+        //Returns the value of x raised to the nth power.
+	    //Preconditions: n >= 0
+	    //Postcondition: The computer value is returned.
+
+	    if(n == 0)
+	        return 1;
+	    else
+	        //else here.
+
+    }
+
+
+
+
+    public static void main(String[] args){
+
+
+    }
+
+}
