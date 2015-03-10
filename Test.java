77abb494b434f01655f3c5277340a94e0886b78a
Jaken Herman <jch053@shsu.edu>
2015-03-10 03:24:08 -0500

    power1
    power1 completed, formatted, and commented.

--- Test.java
+++ Test.java
@@ -2,37 +2,30 @@ import java.util.Scanner;
 
 public class Test {
 
-    public static int power1(int x, int n){
-        //Returns the value of x raised to the nth power.
-	    //Preconditions: n >= 0
-	    //Postcondition: The computer value is returned.
-
-	    if(n == 0)
-        {
-	        return 1;
-        }
-        
-        else
-        {
-            int pow = 1;
-            
-            for(int i = 0; i < n; i++)
-            {
-                pow *= x;
-            }
-        
-            return pow;
-            
-        }
-        
+public static int power1(int x, int n)
+{
+    int total = 1; //initialize total to 1 for n = 0
+    for(int i = 0; i < n; i++)
+    {
+        total = (x * total);
     }
+    return total; // total remains 1 if n = 0
+}
 
 
 
 
     public static void main(String[] args){
 
-        
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Enter X: ");
+		int x = scan.nextInt();
+		System.out.println("Enter N: ");
+		int n = scan.nextInt();
+
+		System.out.println(power1(x, n));
+
     }
 
 }
